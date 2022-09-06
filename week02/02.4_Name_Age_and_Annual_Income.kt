b565d2a4e4dc5e7ed58d0b3f521e3c55ae3e5b19
Chronicle Dog <110718642+TheChronicleDog@users.noreply.github.com>
2022-09-06 09:10:10 -0500

    Create 02.4_Name_Age_and_Annual_Income.kt

---
+++ week02/02.4_Name_Age_and_Annual_Income.kt
@@ -0,0 +1,7 @@
+fun main() {
+   var name : String = "Jimmy Johns"
+   var age : Int = 39
+   var annualPay : Double = 80000.0
+   println("My name is ${name}, I am ${age} years old")
+   println("I hope to earn $${annualPay} per year")
+}
